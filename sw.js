41280f8b06a1e92722c3eb1cb431b2c337592474
Ch1mmyS <kimrekvik.kr@gmail.com>
2026-01-20 03:15:47 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "grenland-live-sport-v1";
+const CACHE_NAME = "grenland-live-sport-v3";
 
 const ASSETS = [
   "/",
@@ -7,16 +7,25 @@ const ASSETS = [
   "/app.js",
   "/manifest.json",
 
-  // Datafiler
+  // Datafiler (alle dine)
+  "/data/champions.json",
   "/data/eliteserien.json",
+  "/data/laliga.json",
   "/data/obos.json",
   "/data/premier_league.json",
-  "/data/champions.json",
-  "/data/laliga.json",
-  "/data/vm2026.json",
-  "/data/vintersport.json",
-  "/data/handball_vm_2026_menn.json",
   "/data/handball_vm_2026_damer.json",
+  "/data/handball_vm_2026_menn.json",
+  "/data/vintersport_kvinner.json",
+  "/data/vintersport_menn.json",
+  "/data/vm2026.json",
+
+  "/data/event_sources.json",
+  "/data/pubs.json",
+  "/data/jam_rules.json",
+  "/data/quiz_rules.json",
+
+  // (football.json ligger, men brukes ikke)
+  "/data/football.json",
 
   // Ikoner
   "/icons/icon-192.png",
@@ -25,7 +34,9 @@ const ASSETS = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
@@ -41,6 +52,7 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => {
       if (cached) return cached;
+
       return fetch(event.request).then((res) => {
         const copy = res.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
